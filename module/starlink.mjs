e6cbbbbda37ceeb1f602ae0032667aba4cf5fde7
TheXeler <1377266215@qq.com>
2025-08-19 02:25:46 +0800

    build11 update build workflow

--- module/starlink.mjs
+++ module/starlink.mjs
@@ -2,22 +2,26 @@ import STARLINK from "./config.mjs"
 
 import * as sheets from "./sheet/module.mjs"
 import * as dataModels from "./data/module.mjs"
+//import * as documents from "./document/module.mjs"
 import {registerModuleData} from "./utils.mjs";
 
 globalThis.starlink = {
     config: STARLINK,
-    dataModels: dataModels
+    dataModels: dataModels,
+    //documents: documents
 }
 
 Hooks.once("init", function () {
     globalThis.starlink = game.starlink = Object.assign(game.system, globalThis.starlink);
 
     CONFIG.STARLINK = STARLINK;
+    Object.assign(CONFIG.Actor.dataModels, dataModels.actor);
+    Object.assign(CONFIG.Item.dataModels, dataModels.item);
 
     registerModuleData();
 
-    CONFIG.Actor.dataModels = dataModels.actor;
-    CONFIG.Item.dataModels = dataModels.item;
+    //CONFIG.Actor.dataModels = dataModels.actor;
+    //CONFIG.Item.dataModels = dataModels.item;
 
     const DocumentSheetConfig = foundry.applications.apps.DocumentSheetConfig;
     DocumentSheetConfig.unregisterSheet(Actor, "core", foundry.appv1.sheets.ActorSheet);
