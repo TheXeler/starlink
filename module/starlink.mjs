f2218210b1d12a1f22acc2cf1fd7d1a9558fcb22
TheXeler <1377266215@qq.com>
2025-08-19 09:59:27 +0800

    build15

--- module/starlink.mjs
+++ module/starlink.mjs
@@ -3,7 +3,7 @@ import STARLINK from "./config.mjs"
 import * as sheets from "./sheet/module.mjs"
 import * as dataModels from "./data/module.mjs"
 //import * as documents from "./document/module.mjs"
-import {registerModuleData} from "./utils.mjs";
+import {log, registerModuleData} from "./utils.mjs";
 
 globalThis.starlink = {
     config: STARLINK,
@@ -15,6 +15,8 @@ Hooks.once("init", function () {
     globalThis.starlink = game.starlink = Object.assign(game.system, globalThis.starlink);
 
     CONFIG.STARLINK = STARLINK;
+
+    log("Data assign...")
     Object.assign(CONFIG.Actor.dataModels, dataModels.actor);
     Object.assign(CONFIG.Item.dataModels, dataModels.item);
 
@@ -23,15 +25,16 @@ Hooks.once("init", function () {
     //CONFIG.Actor.dataModels = dataModels.actor;
     //CONFIG.Item.dataModels = dataModels.item;
 
+    log("Registering sheets...")
     const DocumentSheetConfig = foundry.applications.apps.DocumentSheetConfig;
     DocumentSheetConfig.unregisterSheet(Actor, "core", foundry.appv1.sheets.ActorSheet);
     DocumentSheetConfig.registerSheet(Actor, "starlink", sheets.Ship, {
-        types: ["starlink.ship"],
+        types: [dict.Ship],
         makeDefault: true,
         label: "STARLINK.Sheet.Ship"
     });
     DocumentSheetConfig.registerSheet(Actor, "starlink", sheets.Character, {
-        types: ["starlink.character"],
+        types: ["starlink.Character"],
         makeDefault: true,
         label: "STARLINK.Sheet.Character"
     });
